f18187b8ce34cb850dab9057fc36206b75b69c46
Johan Thijs <jthijs@joes.nl>
2013-08-19 19:32:47 +0200

    testing version for drag and zoom

---
+++ html/js/RaphaelWrapElement.js
@@ -0,0 +1,60 @@
+/**
+ *  Raphaël-ZPD: A zoom/pan/drag plugin for Raphaël.
+ * ==================================================
+ *
+ * This code is licensed under the following BSD license:
+ * 
+ * Copyright 2010 Daniel Assange <somnidea@lemma.org> (Raphaël integration and extensions). All rights reserved.
+ * Copyright 2009-2010 Andrea Leofreddi <a.leofreddi@itcharm.com> (original author). All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ * 
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ * 
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY Andrea Leofreddi ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Andrea Leofreddi OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of Andrea Leofreddi.
+ */
+
+ /*
+Edited bij Johan Thijs to make it compatible with hammer.js and mobile devices
+ */
+
+(function() {
+	var initialized = false;
+
+
+	function init(paper) {
+		var root = paper.canvas;
+
+		var g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+		g.id = 'viewport';
+		root.appendChild(g);
+		paper.canvas = g;
+
+
+
+
+	}
+
+	Raphael.fn.ZPD = function() {
+		if (!initialized) init(this);
+		return this;
+	}
+})();
