cc3edc2e3ea9157767260960bbe237aeb9e57c0c
Rutger <rutger@localhost.localdomain>
2013-08-25 17:08:32 +0200

    temp commit for pulling latest data

--- html/js/app.js
+++ html/js/app.js
@@ -764,7 +764,7 @@ function applyFilter(e, key, mru){
 
 	//set global current mru
 	objPageVars.current_mru = mru;
-	updateWorldmap();	
+	
 	//remove all selected classes
 	var arrAllLi = getEl('filter_container').getElementsByTagName('li');
 	for ( var a = 0; a < arrAllLi.length; a++) {
@@ -784,6 +784,7 @@ function applyFilter(e, key, mru){
 		}
 	}
 	objPageVars.current_sector = getSectorFromBreadCrumb(getMruFilterBreadcrumb());
+	
 	//debugger;
 	//set selected class on clicked element
 	elClicked.className='selected';
@@ -795,6 +796,7 @@ function applyFilter(e, key, mru){
 			height: 'auto'
 		});		
 	}
+	updateWorldmap();	
 }
 
 
