c08b9c4475f5cfb7d2e4d18fe92229a3637302b9
Johan Thijs <johan.thijs@philips.com>
2016-09-15 10:14:43 +0000

    Avoid tweening the filter panel when it's already tweening, do not blink the filter on the public website when a user clicks on a filter

--- html/js/panels/filter.js
+++ html/js/panels/filter.js
@@ -14,16 +14,15 @@ var objFilter = {
     // For the public websites we do not show the overlay in combination with the filter
     if (!isPublicSite()) objOverlay.show();
 
-    self.state.tweening = true;
-    self.el.wrapper.style.display = 'block';
-    TweenLite.to(self.el.wrapper, 0.3, {
-      opacity: 1,
-      onComplete: function () {
-        //debugger;
-        self.state.tweening = false;
-        self.state.visible = true;
-      }
-    });
+    // Animate the panel in view
+    if (!self.state.tweening){
+      self.state.tweening = true;
+      self.el.wrapper.style.display = 'block';
+      TweenLite.to(self.el.wrapper, 0.3, {
+        opacity: 1,
+        onComplete: self.showcomplete
+      });
+    }
 
     //set the mru and oru filter objects to the current state
     objMruFilter.setmrufilterstate();
@@ -37,42 +36,52 @@ var objFilter = {
       objStore.setlocalstorageitem('seenFilterIntro', 'true');
     }
   },
+  showcomplete: function(){
+    objFilter.state.tweening = false;
+    objFilter.state.visible = true;
+  },
   hide: function () {
     var self = this;
-    self.state.tweening = true;
-    TweenLite.to(self.el.wrapper, 0.3, {
-      opacity: 0,
-      onComplete: function () {
-        // For the public websites we do not show the overlay in combination with the filter
-        if (!isPublicSite()) objOverlay.hide();
-        self.el.filtercontent.innerHTML = self.state.currentfilterhtml;
-        self.el.wrapper.style.display = 'none';
-        self.state.tweening = false;
-        self.state.visible = false;
-        self.el.btnapply.style.display = 'none';
-      }
-    });
+    if (!self.state.tweening) {
+      self.state.tweening = true;
+      TweenLite.to(self.el.wrapper, 0.3, {
+        opacity: 0,
+        onComplete: function () {
+          // For the public websites we do not show the overlay in combination with the filter
+          if (!isPublicSite()) objOverlay.hide();
+          self.el.filtercontent.innerHTML = self.state.currentfilterhtml;
+          self.el.wrapper.style.display = 'none';
+          self.state.tweening = false;
+          self.state.visible = false;
+          self.el.btnapply.style.display = 'none';
+        }
+      });
+    }
   },
   blink: function () {
     var self = this;
 
-    TweenLite.to(self.el.filtercontent, 0.2, {
-      opacity: 0.5,
-      onComplete: function () {
-        //debugger;
-        self.el.loader.style.display = 'block';
-        TweenLite.to(self.el.filtercontent, 0.4, {
-          opacity: 1,
-          onComplete: function () {
-            //debugger;
-            self.el.loader.style.display = 'none';
-            //if(self.el.btnapply.style.display == 'none'){
-            self.el.btnapply.style.display = 'block';
-            //}
-          }
-        });
-      }
-    });
+    if (isPublicSite()) {
+      // Do not blink the filter panel on the publick website when a user clicks on it
+      self.blinkcomplete();
+    } else {
+      TweenLite.to(self.el.filtercontent, 0.2, {
+        opacity: 0.5,
+        onComplete: function () {
+          self.el.loader.style.display = 'block';
+          TweenLite.to(self.el.filtercontent, 0.4, {
+            opacity: 1,
+            onComplete: self.blinkcomplete
+          });
+        }
+      });
+    }
+
+  },
+  blinkcomplete: function(){
+    //debugger;
+    objFilter.el.loader.style.display = 'none';
+    objFilter.el.btnapply.style.display = 'block';
   },
   applyfilter: function () {
     var self = this;
